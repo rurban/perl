9dfe9ff6b1d8b4483c8de69910e5adde62bd71ad
Reini Urban <rurban@x-ray.at>
2010-08-30 23:34:17 +0200

    Revert 903fd87c Remove OA_RETINTEGER, unused since 2002 (commit e7311069df54baa6) Revert e7311069 Re: Modulus operator inconsistency and fix overflows for i_multiply only. Do not sacrifice the whole integer optimizations for one corner case, which can be easily detected.
    t/lib/warnings: changed from
      Use of uninitialized value in addition (+)
    to
      Use of uninitialized value in integer addition (+)
    Fixing that in the next commitB

--- op.c
+++ op.c
@@ -2640,6 +2640,31 @@ S_fold_constants(pTHX_ register OP *o)
     return newop;
 
  nope:
+    if (!(PL_opargs[type] & OA_OTHERINT))
+	return o;
+
+    if (!(PL_hints & HINT_INTEGER)) {
+	if (type == OP_MODULO
+	    || type == OP_DIVIDE
+	    || type == OP_MULTIPLY
+	    || !(o->op_flags & OPf_KIDS))
+	{
+	    return o;
+	}
+
+	for (curop = ((UNOP*)o)->op_first; curop; curop = curop->op_sibling) {
+	    if (curop->op_type == OP_CONST) {
+		if (SvIOK(((SVOP*)curop)->op_sv))
+		    continue;
+		return o;
+	    }
+	    if (PL_opargs[curop->op_type] & OA_RETINTEGER)
+		continue;
+	    return o;
+	}
+	o->op_ppaddr = PL_ppaddr[++(o->op_type)];
+    }
+
     return o;
 }
 
